a6c06ba493a20906e7e8541a1662602ca33b6f8e
Piotr Sarnacki <drogus@gmail.com>
2013-06-28 17:37:34 +0200

    Rename deep_symbolize_keys to _deep_symbolize_keys
    Rails implemented deep_symbolize_keys, but without resolving arrays, so
    we need our own implementation. I'm renaming for now to avoid conflicts

--- lib/core_ext/hash/deep_symbolize_keys.rb
+++ lib/core_ext/hash/deep_symbolize_keys.rb
@@ -1,19 +1,22 @@
 class Hash
-  def deep_symbolize_keys
+  def _deep_symbolize_keys
     inject({}) { |result, (key, value)|
-      result[(key.to_sym rescue key) || key] = case value
-      when Array
-        value.map { |value| value.is_a?(Hash) ? value.deep_symbolize_keys : value }
-      when Hash
-        value.deep_symbolize_keys
+      result[(key.to_sym rescue key) || key] = if value.respond_to?(:_deep_symbolize_keys)
+        value._deep_symbolize_keys
       else
         value
       end
       result
     }
-  end unless Hash.method_defined?(:deep_symbolize_keys)
+  end
 
-  def deep_symbolize_keys!
-    replace(deep_symbolize_keys)
-  end unless Hash.method_defined?(:deep_symbolize_keys!)
+  def _deep_symbolize_keys!
+    replace(_deep_symbolize_keys)
+  end
+end
+
+class Array
+  def _deep_symbolize_keys
+    map { |value| value.respond_to?(:_deep_symbolize_keys) ? value._deep_symbolize_keys : value }
+  end
 end
