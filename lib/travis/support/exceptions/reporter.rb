1e140fd9d7378294600989062926ba65730111de
Sven Fuchs <me@svenfuchs.com>
2013-08-12 17:52:43 +0200

    Split out Raven and Logger adapters for exception reporting
    On te we need exceptions to go to logs. This changes things to
    be based on configuration.

--- lib/travis/support/exceptions/reporter.rb
+++ lib/travis/support/exceptions/reporter.rb
@@ -15,13 +15,11 @@ module Travis
           queue.push(error)
         end
 
-        def enabled?
-          @enabled ||= begin
-            require 'raven'
-            !!Travis.config.sentry.dsn
-          rescue LoadError => e
-            false
-          end
+        def adapter
+          Travis.config.sentry.dsn ? Adapter::Raven : Adapter::Logger
+        rescue LoadError => e
+          Travis.logger.error 'Could not load raven, falling back to logger for exception reporting'
+          Adapter::Logger.new
         end
       end
 
@@ -31,14 +29,6 @@ module Travis
       attr_accessor :thread
 
       def run
-        if enabled?
-          ::Raven.configure do |config|
-            config.dsn = Travis.config.sentry.dsn
-            config.ssl = Travis.config.ssl if Travis.config.ssl
-            config.logger = Travis.logger  if Travis.logger
-            config.current_environment = Travis.env
-          end
-        end
         @thread = Thread.new &method(:error_loop)
       end
 
@@ -52,9 +42,7 @@ module Travis
       end
 
       def handle(error)
-        Travis.logger.error(message_for(error))
-        options = { extra: metadata_for(error) }
-        Raven.capture_exception(error, options) if enabled?
+        adapter.handle(error, extra: metadata_for(error))
       rescue Exception => e
         puts '---- FAILSAFE ----'
         puts "Error while handling exception: #{e.message}"
@@ -62,22 +50,12 @@ module Travis
         puts '------------------'
       end
 
-      def message_for(error)
-        lines = ["Error: #{error.message}"]
-        lines += error.backtrace ? error.backtrace : [] if Travis.logger.level == Logger::DEBUG
-        lines.join("\n")
+      def adapter
+        @adapter ||= self.class.adapter.new(Travis.config, Travis.logger, env: Travis.env)
       end
 
       def metadata_for(error)
-        metadata = { }
-        metadata.merge!(error.metadata) if error.respond_to?(:metadata)
-        metadata
-      end
-
-      private
-
-      def enabled?
-        self.class.enabled?
+        error.respond_to?(:metadata) ? error.metadata : {}
       end
     end
   end
