654ef6ac2d2b33b5ed45cc303dc6ef522c000307
Mathias Meyer <meyer@paperplanes.de>
2013-11-21 10:37:53 +0100

    Move require for active_record into the connect method.

--- lib/travis/support/database.rb
+++ lib/travis/support/database.rb
@@ -1,12 +1,12 @@
-require 'active_record'
-require 'erb'
-
 # Encapsulates setting up ActiveRecord and connecting to the database as
 # required for travis-hub, which is a non-rails app.
 module Travis
   module Database
     class << self
       def connect
+        require 'active_record'
+        require 'erb'
+
         ActiveRecord::Base.default_timezone = :utc
         ActiveRecord::Base.logger = Travis.logger
 
